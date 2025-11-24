7f9991be81019ee50d62812fc288512f3aa8534b
Larry-lai-smarthon <larry.lai.smarthon@gmail.com>
2025-11-24 12:15:20 +0800

    update PH sensor function to new

--- SmartScience.ts
+++ SmartScience.ts
@@ -251,51 +251,40 @@ namespace SmartScience {
     //------------------BME280----------------------------------------------
     //---PH Sensor-----------------------------------------------------------------
 
-    let ph_value = ""
-    //% blockId="readPH"
-    //% block="Read PH value at %pin"
-    //% weight=80 
-    //% group="PH Sensor"
-    export function readPH(pin: AnalogPin): string {
-        let sensorarray: number[] = []
-        for (let i = 0; i < 10; i++) {
-            sensorarray.push(pins.analogReadPin(AnalogPin.P0))
-            basic.pause(10)
-        }
-        sensorarray.sort((n1, n2) => n1 - n2);
-        for (let value of sensorarray) {
-            serial.writeLine(value.toString())
-        }
-        ph_value = (sensorarray[5] * 5 * 10 * 35 / 1024).toString()
-        serial.writeLine("===========")
-        if (ph_value.length == 3) {
-            serial.writeLine("PH: " + ph_value.substr(0, 1) + "." + ph_value.substr(1, ph_value.length))
-            return ph_value.substr(0, 1) + "." + ph_value.substr(1, ph_value.length)
-        } else {
-            serial.writeLine("PH: " + ph_value.substr(0, 2) + "." + ph_value.substr(2, ph_value.length))
-            return ph_value.substr(0, 2) + "." + ph_value.substr(2, ph_value.length)
-        }
-    }
-
     let ph_value_number = 0
     //% blockId="readPHNumber"
-    //% block="Read PH value (x100) pin %ports| offset %offset"
-    //% weight=70 group="PH Sensor"
+    //% block="Read PH value pin %ports| offset %offset"
+    //% weight=70 group="Water"
     export function readPhNumber(ports: AnalogPin, offset: number): number {
 
         let temp = 0;
         temp = ports
         let sensorarray: number[] = []
+        let avgValue: number = 0;
         for (let i = 0; i < 10; i++) {
             sensorarray.push(pins.analogReadPin(temp))
             basic.pause(10)
         }
         sensorarray.sort((n1, n2) => n1 - n2);
-        for (let value of sensorarray) {
-            serial.writeLine(value.toString())
+        for (let i = 2; i < 8; i++) {
+            avgValue += sensorarray[i];
         }
-        ph_value_number = (sensorarray[5] * 5 * 10 * 35 / 1024) + offset
-        return ph_value_number
+
+        avgValue = avgValue / 6
+        ph_value_number = ((avgValue * 5 / 1024 )*10.623 + offset -23.813)
+        return ph_value_number 
+    }
+
+    let Voltage =0
+    let pH_Value =0
+    //% blockId="readVoltage"
+    //% block="Read Voltage value pin %ports"
+    //% weight=70 group="Water"
+    export function ShowVoltage(ports: AnalogPin): number {
+
+        pH_Value = pins.analogReadPin(ports);
+        Voltage = pH_Value * (5.0 / 1023.0);
+        return Voltage
     }
     //---PH Sensor-----------------------------------------------------------------
     //--CO2 and TVOC Sensor (CCS811)----------------------------------------------------
